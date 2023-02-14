8ce68abaa3e135d2e3855d6c56753264df850da1
jonescl1 <jonescl1@RHIT-R912GJXK.rose-hulman.edu>
2023-02-14 16:15:02 -0500

    added salting and hashing Merge branch 'master' of https://github.com/watsonet/moviedb.git into HEAD

--- DataCreation/src/ui/LoginFrame.java
+++ DataCreation/src/ui/LoginFrame.java
@@ -8,16 +8,27 @@ import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.sql.CallableStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Base64;
 import java.util.Properties;
+import java.util.Random;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -27,10 +38,14 @@ public class LoginFrame {
 	private String password;
 	private int width = 400;
 	private int height = 200;
+	
+	private static final Random RANDOM = new SecureRandom();
+	private static final Base64.Encoder enc = Base64.getEncoder();
+	private static final Base64.Decoder dec = Base64.getDecoder();
 
 	public void createFrame() {
 		DatabaseConnectionService dbcs = null;
-		try (InputStream input = new FileInputStream("./DataCreation/src/mediadb.properties")) {
+		try (InputStream input = new FileInputStream("../DataCreation/src/mediadb.properties")) {
 			Properties properties = new Properties();
 			properties.load(input);
 			dbcs = new DatabaseConnectionService(properties.getProperty("serverName"),
@@ -67,52 +82,58 @@ public class LoginFrame {
 				String pass = getPassword();
 				DatabaseConnectionService dbcs = ((DatabaseConnectionService) ((JButton) e.getSource())
 						.getClientProperty("dbcs"));
-				int returnValue = 1;
 				if (user == null || pass == null) {
 					loginLabel.setText("Username or password cannot be empty");
 				} else {
 					loginLabel.setText("Connecting...");
 					try {
-						CallableStatement cs = dbcs.getConnection().prepareCall("{? = call checkUser(?, ?)}");
-						cs.setString(2, user);
-						cs.setString(3, pass);
-						cs.registerOutParameter(1, Types.INTEGER);
-						cs.execute();
-						returnValue = cs.getInt(1);
+						CallableStatement cs = dbcs.getConnection().prepareCall("{call checkUser(?)}");
+						cs.setString(1, user);
+						ResultSet rs = cs.executeQuery();
+						int PWSalt = rs.findColumn("PasswordSalt");
+						int PWHash = rs.findColumn("Password");
+						while(rs.next()) {
+							String hash = rs.getString(PWHash);
+							String saltStr = rs.getString(PWSalt);
+							byte[] salt = dec.decode(saltStr);
+							
+							if(hashPassword(salt, pass).compareTo(hash) == 0) {
+								loginFrame.dispose();
+								
+								Main.currentUser = getUsername();
+	
+								MainFrame mainFrame = new MainFrame(dbcs);
+								mainFrame.createFrame();
+							}
+						}
 					} catch (SQLException e1) {
 						e1.printStackTrace();
 					}
-
-					if (returnValue > 0) {
-						loginLabel.setText("Failed to connect, try again.");
-						return;
-					}
-
-					loginFrame.dispose();
-					
-					Main.currentUser = getUsername();
-
-					MainFrame mainFrame = new MainFrame(dbcs);
-					mainFrame.createFrame();
+					loginLabel.setText("Failed to connect, try again.");
 				}
 			}
 		});
 		creation.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				byte[] salt = getNewSalt();
 				String user = getUsername();
 				String pass = getPassword();
+				
 				DatabaseConnectionService dbcs = ((DatabaseConnectionService) ((JButton) e.getSource())
 						.getClientProperty("dbcs"));
 				int returnValue = -1;
 				if (pass != null && pass.length() > 20) {
 					loginLabel.setText("Password must be less than 20 characters!");
 				} else {
+					String SaltAndHash = hashPassword(salt, pass);
 					try {
-						CallableStatement cs = dbcs.getConnection().prepareCall("{? = call addUser(?, ?)}");
+						CallableStatement cs = dbcs.getConnection().prepareCall("{? = call addUser(?, ?, ?)}");
 						cs.setString(2, user);
-						cs.setString(3, pass);
+						cs.setString(3, SaltAndHash);
+						cs.setString(4, getStringFromBytes(salt));
 						cs.registerOutParameter(1, Types.INTEGER);
+						
 						cs.execute();
 						returnValue = cs.getInt(1);
 					} catch (SQLException e1) {
@@ -213,5 +234,32 @@ public class LoginFrame {
 
 	public String getPassword() {
 		return this.password;
+	}
+	public byte[] getNewSalt() {
+		byte[] salt = new byte[16];
+		RANDOM.nextBytes(salt);
+		return salt;
+	}
+	
+	public String getStringFromBytes(byte[] data) {
+		return enc.encodeToString(data);
+	}
+
+	public String hashPassword(byte[] salt, String password) {
+
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+		SecretKeyFactory f;
+		byte[] hash = null;
+		try {
+			f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			hash = f.generateSecret(spec).getEncoded();
+		} catch (NoSuchAlgorithmException e) {
+			JOptionPane.showMessageDialog(null, "An error occurred during password hashing. See stack trace.");
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			JOptionPane.showMessageDialog(null, "An error occurred during password hashing. See stack trace.");
+			e.printStackTrace();
+		}
+		return getStringFromBytes(hash);
 	}
 }
